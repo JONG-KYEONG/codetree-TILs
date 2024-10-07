a85cb3ba04eb9ffa295e547a98eaa9bea27e50cb
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-10-07 16:44:06 +0900

    Title: 코드트리 투어, Time: 93ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241007/코드트리 투어/codetree-tour.java
+++ 241007/코드트리 투어/codetree-tour.java
@@ -33,6 +33,7 @@ public class Main {	// 처음 출발지 0,
 			st = new StringTokenizer(br.readLine());
 			int num = Integer.parseInt(st.nextToken());
 			if(num == 100) {
+				isChange = true;
 				n = Integer.parseInt(st.nextToken());
 				m = Integer.parseInt(st.nextToken());
 //				map = new ArrayList[n][n];
@@ -66,7 +67,7 @@ public class Main {	// 처음 출발지 0,
 //						map[y][x] = dis;
 //					}
 				}
-                dijstra();
+//                dijstra();
 			}
 			else if(num == 200) {
 				int id = Integer.parseInt(st.nextToken());
@@ -76,7 +77,7 @@ public class Main {	// 처음 출발지 0,
 				product[id][1] = revenue;
 				product[id][2] = dest;
 				idList.add(id);
-                dijstra();
+//                dijstra();
 			}
 			else if(num == 300) {
 				int id = Integer.parseInt(st.nextToken());
@@ -86,10 +87,13 @@ public class Main {	// 처음 출발지 0,
 					product[id][2] = 0;
 					idList.remove(idList.indexOf(id));
 				}
-				dijstra();
+//				dijstra();
 			}
 			else if(num == 400) {
-				dijstra();
+				if(isChange){
+					dijstra();
+					isChange = false;
+				}
 				int max = Integer.MIN_VALUE;
 				int id = -1;
 				for(Integer i : idList) {
@@ -126,7 +130,7 @@ public class Main {	// 처음 출발지 0,
 			}
 			else {
 				start = Integer.parseInt(st.nextToken());
-				dijstra();
+				isChange = true;
 			}
 		}
 		System.out.print(sb);
